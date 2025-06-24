88360139f3f9e7e7b718127ce5119c1140b9356d
RuLe0G <khlebnikov2015@yandex.ru>
2025-06-24 18:09:29 +0700

    Update wheelData

--- js/data/wheelData.js
+++ js/data/wheelData.js
@@ -2,28 +2,35 @@ export default {
     default: {
         title: "Колесо балдежа",
         items: [
-            {title: "Повысить шанс выпадения игры в чужом колесе", weight: 5},
-            {title: "+25 поинтов", weight: 5},
-            {title: "Реролл", weight: 5},
-            {title: "1 из 2 игр на выбор", weight: 5},
-            {title: "+20% поинтов ", weight: 3},
-            {title: "Колесо с говном противнику", weight: 3},
-            {title: "+50% поинтов", weight: 1},
-            {title: "Безнаказанный дроп", weight: 1},
+            {title: "1 из 2 игр на выбор", weight: 10},
+            {title: "Реролл", weight: 10},
+            {title: "+25 поинтов", weight: 10},
+            {title: "Повысить шанс выпадения игры в чужом колесе", weight: 10},
+            {title: "Колесо с говном противнику", weight: 6},
+            {title: "+10% поинтов", weight: 6},
+            {title: "+5% поинтов за модификатор", weight: 4},
+            {title: "+1 день на прохождение ради колеса балдежа", weight: 4},
+            {title: "+20% поинтов ", weight: 2},
+            {title: "Безнаказанный дроп", weight: 2},
+            {title: "+10 поинтов за каждое выполненное достижение", weight: 2},
         ]
     },
     shame: {
         title: "Колесо говна",
         items: [
-            {title: "-поинты по кубику х10", weight: 5},
-            {title: "Колесо балдежа противнику", weight: 5},
-            {title: "-10 поинтов", weight: 5},
-            {title: "-10% поинтов", weight: 5},
-            {title: "-25% поинтов", weight: 3},
-            {title: "-25 поинтов", weight: 3},
-            {title: "Ролл из отборного говна", weight: 3},
-            {title: "-50% поинтов", weight: 1},
-            {title: "Повышение шанса рекомендаций", weight: 1},
+            {title: "Ролл из отборного говна", weight: 10},
+            {title: "-10% поинтов от следующей игры", weight: 10},
+            {title: "-10 поинтов", weight: 10},
+            {title: "-Колесо балдежа противнику", weight: 10},
+            {title: "-поинты по кубику х10", weight: 10},
+            {title: "-25 поинтов", weight: 6},
+            {title: "-5% от стоимости модификатора", weight: 6},
+            {title: "Обязательный модификатор ", weight: 6},
+            {title: "-10% от стоимости модификатора", weight: 4},
+            {title: "-25% поинтов от следующей игры", weight: 2},
+            {title: "Повышение шанса рекомендаций", weight: 2},
+            {title: "-1 день на прохождение ради колеса балдежа", weight: 2},
+            {title: "-5% поинтов от общего счета", weight: 1}
         ]
     },
     super_shame: {
@@ -33,8 +40,11 @@ export default {
             {title: "The Mercury Man", weight: 1},
             {title: "Poppy Playtime", weight: 1},
             {title: "Hatoful Boyfriend", weight: 1},
-            {title: "5 каток в кс", weight: 1},
-            {title: "5 каток в доту", weight: 1},
+            {title: "5 каток в кс (66 поинтов)", weight: 1},
+            {title: "5 каток в доту (66 поинтов)", weight: 1},
+            {title: "Postal 3", weight: 1},
+            {title: "Смута (124 поинта)", weight: 1},
+            {title: "Sniper Art of Victory", weight: 1},
         ]
     }
 };
