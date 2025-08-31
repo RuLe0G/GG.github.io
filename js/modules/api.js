84439777a050e788d5349b0630db86bd64ebc80c
RuLe0G <khlebnikov2015@yandex.ru>
2025-09-01 04:12:27 +0700

    UPD

--- js/modules/api.js
+++ js/modules/api.js
@@ -19,14 +19,6 @@ class Api {
             throw error;
         }
     }
-    static clear() {
-        try {
-            Cache.clearAll();
-        } catch (error) {
-            console.error('clear all error:', error);
-            throw error;
-        }
-    }
 }
 
 export default Api;
--- js/modules/cache.js
+++ js/modules/cache.js
@@ -24,13 +24,4 @@ export class Cache {
             console.error(`Ошибка при очистке кэша для ${key}:`, error);
         }
     }
-
-    static clearAll() {
-        try {
-            localStorage.clear();
-            console.log("Все данные localStorage удалены");
-        } catch (error) {
-            console.error(`Ошибка при очистке всего кэша:`, error);
-        }
-    }
 }
