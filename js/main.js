8b8ac1061b8e9d0706071f7c42637e41e1149915
RuLe0G <khlebnikov2015@yandex.ru>
2026-06-29 02:04:13 +0700

    ClearCache

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ window.clearCache = () => {
 
 class App {
     constructor() {
+        Cache.clearAll()
         this.tabs = {};
         this.currentTabId = 'tab1';
         this.attachedFeedbackFile = null; 
